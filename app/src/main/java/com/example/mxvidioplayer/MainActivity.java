61c7806548ccfe096f2862b0580a630c472fea0a
Nashrul Saefullah <nashrul.saefullah.ft18@mail.umy.ac.id>
2020-07-07 08:15:35 +0700

    edit in main Activity java

--- app/src/main/java/com/example/mxvidioplayer/MainActivity.java
+++ app/src/main/java/com/example/mxvidioplayer/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.mxvidioplayer;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.khizar1556.mkvideoplayer.MKPlayerActivity;
+
 import net.alhazmy13.mediapicker.Video.VideoPicker;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +27,16 @@ public class MainActivity extends AppCompatActivity {
                 .extension(VideoPicker.Extension.MP4)
                 .enableDebuggingMode(true)
                 .build();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == VideoPicker.VIDEO_PICKER_REQUEST_CODE && resultCode == RESULT_OK){
+            List<String> mPaths = data.getStringArrayListExtra(VideoPicker.EXTRA_VIDEO_PATH);
+
+            MKPlayerActivity.configPlayer(this).play(mPaths.get(0));
+        }
     }
 }
